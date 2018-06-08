e25c310d222e7ec35a1c9c6e5bd287f2609e9975
maks.skr.j@gmail.com <melkiy230898gutter>
2018-06-08 15:54:38 +0300

    practice 9 completed

--- practice9/src/main/Java/Vote.java
+++ practice9/src/main/Java/Vote.java
@@ -6,15 +6,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
 
 @WebServlet("/vote")
 public class Vote extends HttpServlet {
 
+    {
+        try {
+            init();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
     @Override
-    public void init(ServletConfig config) throws ServletException {
-        ServletContext context = config.getServletContext();
+    public void init() throws ServletException {
+        ServletContext context = getServletContext();
         Map<String, Integer> votes = new HashMap<>();
         votes.put("Football", 0);
         votes.put("Basketball", 0);
@@ -24,15 +33,21 @@ public class Vote extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        doPost(req,resp);
+
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
         String variant = req.getParameter("op");
         HashMap<String, Integer> votes = (HashMap<String, Integer>) req.getSession().getServletContext().getAttribute("votes");
+
+        votes.computeIfPresent(variant, (k, v) -> v + 1);
+
         req.setAttribute("votes", votes);
         req.getRequestDispatcher("resultVote.jsp").forward(req, resp);
+
 
     }
 }
--- practice9/src/main/webapp/resultVote.jsp
+++ practice9/src/main/webapp/resultVote.jsp
@@ -1,9 +1,35 @@
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE html>
+<%@ page import="java.util.*" %>
+<%@ page contentType="text/html;charset=UTF-8"%>
 <html>
 <head>
-    <title>Title</title>
+    <head>
+        <title>Statistics</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </head>
 </head>
-<body>
+<table class="table table-bordered">
+    <thead>
+    <tr>
+        <th scope="col">System</th>
+        <th scope="col">Votes</th>
+    </tr>
+    </thead>
 
+    <tbody>
+
+        <% Map<String, Integer> map = (Map<String, Integer>)(request.getAttribute("votes"));
+    for (Map.Entry<String, Integer> entry : map.entrySet()) { %>
+    <tr>
+        <td><%=entry.getKey()%></td>
+        <td><%=entry.getValue()%></td>
+    </tr>
+        <% } %>
+</table>
+<a href="indexVote.jsp">Go back to voting</a>
 </body>
 </html>
